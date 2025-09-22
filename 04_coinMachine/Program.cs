b535717b53e1907850664b8af33353d8934f224d
krejcirikova <krejcirikova@spszl.cz>
2025-09-22 13:07:07 +0200

    Add files via upload

---
+++ 04_coinMachine/Program.cs
@@ -0,0 +1,18 @@
+﻿int amount = 420;
+
+/*
+ * Automat na mince.
+ * Napište program, který na obrazovku postupně vypíše nejmenší počet mincí,
+ * na které se dá vstupní obnos (amount) rozměnit.
+ * Mince jsou: 1, 2, 5, 10, 20, 50
+ * Příklad:
+ *   Dostanete:
+ *   8x 50,
+ *   1x 20,
+ *   0x 10,
+ *   0x 5,
+ *   0x 2,
+ *   0x 1
+ *   
+ * Zkuste změnit obnos a ověřit funkčnost vašeho kódu.
+ */
